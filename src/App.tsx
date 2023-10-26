d0a62d50d9de73edc595cd34113009f4457668e0
Léopold MAURICE <leopold.maurice@efrei.net>
2023-10-26 16:38:22 +0200

    challenge done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import whiteOverlay from './assets/white-overlay.svg';
+import heroImage from './assets/hero-image-business-card.png';
+import avatar from './assets/avatar-image-business-card.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cardComponent">
+        <div className='imgPart'>
+          <img className='heroImg' src={heroImage} alt="y'a pas"></img>
+          <img className='overlayImg' src={whiteOverlay} alt="y'a pas"></img>
+        </div>
+        <div className='infomartionPart'>
+          <div className='textContainer'>
+            <div className='title'>Perfect solution for small business</div>
+            <div className='text'>Small businesses need to generate leads to grow.</div>
+            <div className='text'>You can use tools like Ringy.</div>
+          </div>
+          <div className='avatarContainer'>
+            <img className='avatarImg' src={avatar} alt="y'a pas"></img>
+            <div className='textAvatarContainer'>
+              <div className='name'>Amy Burgess</div>
+              <div className='job'>Customer Manger, Solution Oy</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
